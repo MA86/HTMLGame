1da6f488be926aaacec836f483f2411e1b56cc50
unknown <micro.access@hotmail.com>
2021-06-21 22:56:44 -0400

    Organize files.

--- game.js
+++ game.js
@@ -1,83 +1,19 @@
-/* Define tank blueprint */
-const Tank = function(xPos, yPos, rotationSpeed, speed, sprite) {
-  // Initialize all attributes
-  this.position = {
-    "x": xPos,
-    "y": yPos
-  };
-  this.speed = speed;
-  // Pixels per second
-  this.rotation = 0;
-  // Degrees
-  this.rotationSpeed = rotationSpeed;
-  // Degrees per second
-  this.sprite = sprite;
-  // Image object
+"use strict";
 
-  this.render = function(ctx) {
-    let spriteCenter = {
-      "x": this.sprite.naturalWidth / 2,
-      "y": this.sprite.naturalHeight / 2
-    };
-    // Rotate around self
-    ctx.save();
-    ctx.translate(this.position.x + spriteCenter.x, this.position.y + spriteCenter.y);
-    ctx.rotate(this.rotation * Math.PI / 180);
-    ctx.drawImage(this.sprite, -spriteCenter.x, -spriteCenter.y);
-    ctx.restore();
-  }
+import { Tank, Background } from './entities.js';
 
-  this.update = function(keysDown, dt) {
-    let rotationInRadian = this.rotation * Math.PI / 180;
-    let dx = Math.cos(rotationInRadian) * (this.speed * dt);
-    let dy = Math.sin(rotationInRadian) * (this.speed * dt);
-
-    // Move forward/backward
-    if (keysDown.ArrowUp == true) {
-      this.position.x += dx;
-      this.position.y += dy;
-    }
-    if (keysDown.ArrowDown == true) {
-      this.position.x -= dx;
-      this.position.y -= dy;
-    }
-
-    // Rotate right/left
-    if (keysDown.ArrowRight == true) {
-      this.rotation += this.rotationSpeed * dt;
-    }
-    if (keysDown.ArrowLeft == true) {
-      this.rotation -= this.rotationSpeed * dt;
-    }
-    // Wrap around
-    this.rotation = this.rotation % 360;
-  }
-}
-
-/* Define background blueprint */
-const Background = function(bgSprite) {
-  this.bgSprite = bgSprite;
-
-  this.render = function(ctx) {
-    ctx.drawImage(this.bgSprite, 50, 50);
-  }
-
-  this.update = function(keysDown, dt) {// does nothing
-  }
-}
-
-const addKeyboardInputEventListeners = function(dic) {
-  addEventListener("keydown", function(e) {
+const addKeyboardInputEventListeners = function (dic) {
+  addEventListener("keydown", function (e) {
     dic[e.code] = true;
   }, false);
 
-  addEventListener("keyup", function(e) {
+  addEventListener("keyup", function (e) {
     delete dic[e.code];
   }, false);
 }
 
-/* On Window load - aka main */
-window.addEventListener("load", function(e) {
+/* On Window load - AKA main */
+window.addEventListener("load", function (e) {
   // Global variables
   var gameCanvas = document.getElementById("game-canvas");
   var ctx = gameCanvas.getContext("2d");
@@ -96,13 +32,13 @@ window.addEventListener("load", function(e) {
   // Tank entity
   let tankImage = new Image();
   tankImage.src = "images/icon.png";
-  let tank = new Tank(100,100,50,100,tankImage);
+  let tank = new Tank(100, 100, 50, 100, tankImage);
   entities.push(tank)
 
   // Game loop
   var timeNow;
   var timeThen;
-  const main = function() {
+  const main = function () {
     timeNow = Date.now();
     let delta = (timeNow - timeThen) / 1000.0;
 
@@ -111,7 +47,7 @@ window.addEventListener("load", function(e) {
 
     // Update entities
     for (let i = 0; i < entities.length; i++) {
-      entities[i].update(keysDown,delta)
+      entities[i].update(keysDown, delta)
     }
 
     // Render entities
