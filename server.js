d0bd747e98efed61cc05e59fbd9f9d7ba3f64d18
unknown <micro.access@hotmail.com>
2021-07-21 02:10:09 -0400

    Basic socket created.

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-// Import Express (a web framework)
+// Import Express
 const express = require("express");
-// Call to create app
-const app = express();
-// Import/initialize HTTP server (app is called to handle HTTP requests)
-const httpServer = require("http").createServer(app);
+// Handler function
+const httpHandler = express();
+// Create HTTP server (httpHandler handles HTTP requests)
+const httpServer = require("http").createServer(httpHandler);
 
-// Serve index.html file at route '/'
-app.get("/", function (req, res) {
+// Import Socket
+const socket = require("socket.io");
+// Create Socket server (httpServer passed because all websockets start with http message)
+const socketServer = new socket.Server(httpServer);
+
+// Search requested files inside client folder, first
+httpHandler.use(express.static(__dirname + "/client"));
+
+// For GET request at '/', send index.html file
+httpHandler.get("/", function (req, res) {
     res.sendFile(__dirname + "/client/index.html");
 });
 
-// Allow browser to GET files from client repository
-app.use(express.static(__dirname));
-app.use(express.static(__dirname + "/styles"));
+// Socket server is used to send/receive small messages
+socketServer.on("connection", function () {
+    console.log("a user connected");
+});
 
-// Start HTTP server listening on port 8000
+// Start HTTP server, listening on port 8000
 httpServer.listen(8000, function () {
     console.log("HTTP SERVER IS LISTENING ON PORT 8000");
 });
