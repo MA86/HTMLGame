a656abfe375eeb820c9c1272ebeac80f55f5d6a5
unknown <micro.access@hotmail.com>
2021-07-20 23:12:26 -0400

    Basic server up.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-// Import/initialize Express (a web framework)
-const app = require("express")();
+// Import Express (a web framework)
+const express = require("express");
+// Call to create app
+const app = express();
 // Import/initialize HTTP server (app is called to handle HTTP requests)
 const httpServer = require("http").createServer(app);
 
-// Serve index.html file
+// Serve index.html file at route '/'
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/client/index.html");
+});
+
+// Allow browser to GET files from client repository
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/styles"));
+
+// Start HTTP server listening on port 8000
+httpServer.listen(8000, function () {
+    console.log("HTTP SERVER IS LISTENING ON PORT 8000");
 });
