e527cd16232b5935527736a4129b8b2d0d57b8ee
unknown <micro.access@hotmail.com>
2021-10-29 22:45:51 -0400

    Updating asset collection.

---
+++ client/images_and_data/shellData.js
@@ -0,0 +1,37 @@
+let shellData = {
+    "frames": [
+        {
+            "filename": "shell.png",
+            "frame": {
+                "x": 1,
+                "y": 1,
+                "w": 256,
+                "h": 39
+            },
+            "rotated": false,
+            "trimmed": false,
+            "spriteSourceSize": {
+                "x": 0,
+                "y": 0,
+                "w": 256,
+                "h": 39
+            },
+            "sourceSize": {
+                "w": 256,
+                "h": 39
+            }
+        }
+    ],
+    "meta": {
+        "version": "1.0",
+        "image": "spritesheet.png",
+        "format": "RGBA8888",
+        "size": {
+            "w": 258,
+            "h": 41
+        },
+        "scale": "1"
+    }
+}
+
+export { shellData };
