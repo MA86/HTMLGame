0d4d1bb62f25ecc8eeda35e6fed65b2ff1d3fb8e
unknown <micro.access@hotmail.com>
2021-11-18 23:29:37 -0500

    Planning comm. bet. client and server.

--- server_entities/turret.js
+++ server_entities/turret.js
@@ -6,51 +6,56 @@ const Bodies = Matter.Bodies;
 const Composite = Matter.Composite;
 
 class Turret {
-    constructor(initPos, world, parent) {
-        // Create turret on tank's position
+    constructor(initPos, world, socket, parent) {
+        // Create body representing turret
         this.body = Bodies.rectangle(initPos.x, initPos.y, 148, 10, {
-            isSensor: true,     // Inactivate body
+            isSensor: true,     // Inactivate physics
         });
 
         // Properties of turret
         this.speed = 1;
         this.readyToFire = true;
-        this.parent = parent;
+        this.isPlayer = false;
         this.world = world;
+        this.socket = socket;
+        this.parent = parent;
     }
 
-    setupEventListeners(socket) {
-        // Prepare a rotation in radians
+    setupEventListeners() {
+        // Create rotation in radians
         let rotation = 0.00872665 * this.speed;
         let thiss = this;
 
-        // Apply rotation for left/right turn
-        socket.on("rotate right", function (data) {
-            Body.rotate(thiss.body, rotation);
-        });
-        socket.on("rotate left", function (data) {
-            Body.rotate(thiss.body, -rotation);
-        });
+        if (this.isPlayer) {
+            // Trigger left/right turn
+            this.socket.on("rotate right", function (data) {
+                Body.rotate(thiss.body, rotation);
+            });
+            this.socket.on("rotate left", function (data) {
+                Body.rotate(thiss.body, -rotation);
+            });
 
-        socket.on("fire shell", function (data) {
-            if (this.readyToFire) {
-                // Fire a shell
-                let shell = new Shell(
-                    thiss.world,
-                    thiss.body,
-                    {
-                        speed: 0.01,
-                        type: "HE",
-                        blastRadius: 2,
-                        penetration: 2
-                    }
-                );
+            // Trigger turret fire
+            this.socket.on("fire shell", function (data) {
+                if (this.readyToFire) {
+                    // Fire a shell
+                    let shell = new Shell(
+                        thiss.world,
+                        thiss.body,
+                        {
+                            speed: 0.01,
+                            type: "HE",
+                            blastRadius: 2,
+                            penetration: 2
+                        }
+                    );
 
-                // Wait to "load" new shell
-                thiss.readyToFire = false;
-                thiss.setLoadTime(500);
-            }
-        });
+                    // Wait to "load" new shell
+                    thiss.readyToFire = false;
+                    thiss.setLoadTime(500);
+                }
+            });
+        }
     }
 
     // Shell load time
